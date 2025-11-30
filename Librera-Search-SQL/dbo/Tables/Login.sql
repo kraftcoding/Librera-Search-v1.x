65882eebff70d86884a0edaf26863be8c6c103fa
Kraftcoding <i.elg.alzaa@gmail.com>
2025-11-30 14:07:54 -0800

    Login table added to SQl project

---
+++ Librera-Search-SQL/dbo/Tables/Login.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Login] (
+    [id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Email]    NVARCHAR (50)  NOT NULL,
+    [Password] NVARCHAR (MAX) NOT NULL,
+    [Role]     NVARCHAR (50)  NULL,
+    CONSTRAINT [PK_Login] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
