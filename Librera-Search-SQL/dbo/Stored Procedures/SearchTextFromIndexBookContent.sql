32d87ae6a10a6e832c8d8d30ed7be40530a2c694
Kraftcoding <i.elg.alzaa@gmail.com>
2025-12-04 03:33:05 -0800

    SP is added

---
+++ Librera-Search-SQL/dbo/Stored Procedures/SearchTextFromIndexBookContent.sql
@@ -0,0 +1,12 @@
+﻿
+CREATE PROC [dbo].[SearchTextFromIndexBookContent] @SearchStr varchar(128)
+AS
+BEGIN   
+    SET NOCOUNT ON
+	SELECT * FROM IndexedBookContent
+	WHERE TextContent LIKE  '%' + @SearchStr + '%'
+END
+
+-- TEST
+--use LibreraSearch 
+--exec dbo.SearchTextFromIndexBookContent @SearchStr = 'encrypt storage'
